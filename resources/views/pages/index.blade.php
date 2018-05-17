73f042c01198005a781be0d80f4ed4453d592c64
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-05-17 23:19:59 +0800

    Change tagline.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <div class="jumbotron text-center">
     <h1>{{$title}}</h1>
-    <p>This is Application can give information of the status of every banks here in Bacolod City.</p>
+    <p>This Application can provide information with regards to the status of every bank in Bacolod City.</p>
     <p><a href="/login" class="btn btn-primary btn-lg" role="button">Login</a> <a href="/register" class="btn btn-success btn-lg" role="button">Register</a></p>
   </div>
 @endsection

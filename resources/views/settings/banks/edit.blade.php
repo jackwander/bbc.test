a645ef93ef966ac67bc016f384050c79bc0b33a0
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-05-18 16:17:34 +0800

    Create view for banks: Edit.

---
+++ resources/views/settings/banks/edit.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+  <h1>Edit Bank</h1>
+  {!! Form::open(['action' => ['BanksController@update', $bank->bank_id],'method' => 'POST']) !!}
+      <div class="form-group">
+        {{Form::label('fullname','Bank full name')}}
+        {{Form::text('fullname',$bank->fullname, ['class'=>'form-control','placeholder'=>'Bank Full Name'])}}
+      </div>
+      <div class="form-group">
+        {{Form::label('shortname','Bank Short Name')}}
+        {{Form::text('shortname',$bank->shortname, ['class'=>'form-control','placeholder'=>'Bank Short Name'])}}
+      </div>      
+      {{Form::hidden('_method','PUT')}}
+      {{Form::submit('Submit',['class'=>'btn btn-primary'])}}
+  {!! Form::close() !!}
+@endsection

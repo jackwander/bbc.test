275d09e789e7ce49665794ad8e688d0da8ea4f87
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-05-18 16:18:02 +0800

    Set variable for DB tables.

--- app/Bank.php
+++ app/Bank.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bank extends Model
 {
-    //
+    protected $table = 'banks';
+
+    public $primaryKey = 'bank_id';
+    public $timestamps = true;
 }

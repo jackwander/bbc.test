8d0514e88151bc1913984a8f5b8627cc392670d8
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-05-18 16:59:32 +0800

    Modify view of Banks: index.

--- resources/views/settings/banks/index.blade.php
+++ resources/views/settings/banks/index.blade.php
@@ -1,25 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>Banks</h1>
+  <h1>Banks</h1> <a href="banks/create" class="btn btn-secondary"><i class="fas fa-plus"></i></a>
   @if(count($banks) > 0)
-      @foreach ($banks as $bank)
         <div class="card">
           <div class="card-body">
-            <div class="row">
-              <div class="col-md-4 col-sm-4">
-                {{-- <img style="width: 100%" src="/storage/cover_images/{{$bank->cover_image}}"> --}}
-              </div>
-              <div class="col-md-8 col-sm-8">
-                <h3><a href="/settings/banks/{{$bank->bank_id}}">{{$bank->name}}</a></h3>
-                <small>Added on {{$bank->created_at}}</small>
-              </div>
-            </div>
+            <table class="table table-striped">
+              <thead>
+                <th>Banks</th>
+                <th><i class="fas fa-cog fa-spin"></i></th>
+              </thead>
+              <tbody>
+                @foreach ($banks as $bank)
+                  <tr>
+                    <td>{{$bank->fullname}} ({{$bank->shortname}})</td>
+                    <td>
+                      <a href="banks/{{$bank->bank_id}}/edit" class="btn btn-primary"><i class="fas fa-edit"></i></a>
+                      <a href="#" class="btn btn-danger" data-toggle="modal" data-target="#deleteBank{{$bank->bank_id}}"><i class="fas fa-trash"></i></a>
+                      <div class="modal fade" id="deleteBank{{$bank->bank_id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h5 class="modal-title" id="exampleModalLabel">Delete <b>{{$bank->fullname}}</b></h5>
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <div class="modal-body">
+                              Are you sure to delete <b>{{$bank->fullname}}</b>? When you click <b>Yes</b> there is no turning back.
+                            </div>
+                            <div class="modal-footer">
+                                {!!Form::open(['action'=>['BanksController@destroy',$bank->bank_id],'method'=>'POST'])!!}
+                                {{Form::hidden('_method','DELETE')}}
+                                {{Form::submit('Yes',['class'=>'btn btn-danger'])}}
+                                {!!Form::close()!!}
+                              {{-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> --}}
+                            </div>
+                          </div>
+                        </div>
+                      </div>                  
+                    </td>
+                  </tr>
+                @endforeach
+              </tbody>
+            </table>
           </div>
         </div>
-      @endforeach
       {{$banks->links()}}
   @else
-      <p>No Locations Found</p>
+      <p>No Banks Found</p>
   @endif
 @endsection

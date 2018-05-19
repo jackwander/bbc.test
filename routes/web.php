801102f5066a53fd0d147303b401e2e210996ea3
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-05-20 02:48:41 +0800

    Create routes for branches.

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,22 @@ Route::resource('/settings/locations','LocationsController');
 Route::resource('/settings/banks','BanksController');
 
 
+// Route::get('/settings/banks/{bank_id}/locations/{location_id}', [
+//     'as' => 'branches.show', 
+//     'uses' => 'BranchesController@branchesLocation'
+// ]);
+
+/*
+ * For Branches Route 
+ */
+Route::resource('/settings/branches','BranchesController');
+Route::get('settings/branches/locations/{location_id}', 'BranchesController@pickBank');
+Route::get('settings/branches/locations/{location_id}/banks/{bank_id}', 'BranchesController@showBranches');
+Route::get('branches/add/{location_id}&{bank_id}','BranchesController@create')->name('settings.branches.create');
+Route::get('settings/branches/status/{status}/branch/{branch_id}', 'BranchesController@editBranchStatus');
+
+
+
 Auth::routes();
 
 Route::get('/dashboard', 'DashboardController@index');

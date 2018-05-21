d42801d42192c53b905054c55e4e7b64581296a8
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-05-22 01:26:47 +0800

    Create route for UserBranches.

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,13 @@ Route::get('/', 'PagesController@index');
 Route::resource('/settings/locations','LocationsController');
 Route::resource('/settings/banks','BanksController');
 Route::resource('/settings/users','UsersController');
-
-// Route::get('/settings/banks/{bank_id}/locations/{location_id}', [
-//     'as' => 'branches.show', 
-//     'uses' => 'BranchesController@branchesLocation'
-// ]);
+Route::resource('/settings/userbranches','UserBranchesController');
 
 /*
  * For Branches Route 
  */
 Route::resource('/branches','BranchesController');
 
-Route::resource('/branches','BranchesController');
 Route::get('branches/locations/{location_id}', 'BranchesController@pickBank');
 Route::get('branches/locations/{location_id}/banks/{bank_id}', 'BranchesController@showBranches');
 Route::get('branches/add/{location_id}&{bank_id}','BranchesController@create')->name('branches.create');

28b35090df223b7ce6f3db4add07b3579f714b23
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-05-22 01:23:46 +0800

    Remove modal confirmation.

--- resources/views/settings/branches/branches.blade.php
+++ resources/views/settings/branches/branches.blade.php
@@ -60,7 +60,7 @@
                   </div>
 
                   <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#editBranch{{$branch->branch_id}}"><i class="fas fa-edit"></i></a>
-                
+
                   <!--Modal For Edit-->
                   <div class="modal fade" id="editBranch{{$branch->branch_id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog modal-lg" role="document">
@@ -74,52 +74,35 @@
                           {!! Form::open(['action' => ['BranchesController@update', $branch->branch_id],'method' => 'POST']) !!}
                           {{Form::hidden('_method','PUT')}}
                         <div class="modal-body">
-                              <div class="form-group">
-                                {{Form::label('branch','Branch')}}
-                                {{Form::text('branch',$branch->branch_name, ['class'=>'form-control','placeholder'=>'Branch'])}}
-                              </div>      
-                        <div class="form-group">
-                          {{Form::label('contactnum','Contact Number')}}
-                          {{Form::text('contactnum',$branch->contactnum, ['class'=>'form-control','placeholder'=>'(Area code)+Contact Number'])}}
-                          <span style="color:red;">X</span> If more than 1 Contact Number. Seperate it with a semi-colon.
-                            @if ($errors->has('contactnum'))
-                                <span class="badge badge-warning">
-                                    <strong>{{ $errors->first('contactnum') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                        <div class="form-group">
-                          {{Form::label('address','Address')}}
-                          {{Form::textarea('address',$branch->address, ['class'=>'form-control','placeholder'=>'Address'])}}
-                        </div> 
-                                <div class="modal fade" id="submitForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                  <div class="modal-dialog modal-sm" role="document">
-                                    <div class="modal-content">
-                                      <div class="modal-header">
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                          <span aria-hidden="true">&times;</span>
-                                        </button>
-                                      </div>
-                                      <div class="modal-body">
-                                          <center><p><h5>Are you sure to submit this?</h5></p></center>
-                                      </div>
-                                      <div class="modal-footer">
-                                        {{Form::submit('Yes',['class'=>'btn btn-primary'])}}
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>        
+                          <div class="form-group">
+                            {{Form::label('branch','Branch')}}
+                            {{Form::text('branch',$branch->branch_name, ['class'=>'form-control','placeholder'=>'Branch'])}}
+                          </div>
+                          <div class="form-group">
+                            {{Form::label('contactnum','Contact Number')}}
+                            {{Form::text('contactnum',$branch->contactnum, ['class'=>'form-control','placeholder'=>'(Area code)+Contact Number'])}}
+                            <span style="color:red;">X</span> If more than 1 Contact Number. Seperate it with a semi-colon.
+                              @if ($errors->has('contactnum'))
+                                  <span class="badge badge-warning">
+                                      <strong>{{ $errors->first('contactnum') }}</strong>
+                                  </span>
+                              @endif
+                          </div>
+                          <div class="form-group">
+                            {{Form::label('address','Address')}}
+                            {{Form::textarea('address',$branch->address, ['class'=>'form-control','placeholder'=>'Address'])}}
+                          </div>
+
                         </div>
                         <div class="modal-footer">
-                          {{-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> --}}
-                                <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#submitForm">Submit</a>
-                          {!! Form::close() !!}
+                            {{Form::submit('Save',['class'=>'btn btn-primary'])}}
+                            {!! Form::close() !!}
                         </div>
                       </div>
                     </div>
                   </div>
                   <!--End of Modal For Edit-->
-                  
+
 
                   @if (!Auth::guest())
                   <a href="#" class="btn btn-danger" data-toggle="modal" data-target="#deleteBranch{{$branch->branch_id}}"><i class="fas fa-trash"></i></a>

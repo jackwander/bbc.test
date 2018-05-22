337fb324105c64b7462857597579c18e82292988
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-05-22 23:45:46 +0800

    Change title.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,7 +9,7 @@ class PagesController extends Controller
 {
     public function index() {
       $banks = Bank::orderBy('fullname','ASC')->get();
-      $title = "Welcome to Bacolod Bank Checker!";
+      $title = "Welcome to Negros Bank Checker!";
       return view('pages.index')->with(['banks'=>$banks,'title'=>$title]);
     }
 }

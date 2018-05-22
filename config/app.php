67064fb1a7fc44a8408c440a5fb98cb3e8e4014a
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-05-22 16:48:47 +0800

    Install Datatables.

--- config/app.php
+++ config/app.php
@@ -147,6 +147,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Yajra\Datatables\DatatablesServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
@@ -209,7 +211,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,        
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Datatables' => Yajra\Datatables\Facades\Datatables::class,        
 
     ],
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,11 @@
     <!--Font Awesome-->
     <link rel="stylesheet" type="text/css" href="{{ asset('plugins/font-awesome/web-fonts-with-css/css/fontawesome-all.css')}}">
     
+    <!--Datatables-->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.16/af-2.2.2/datatables.min.css"/>
+ 
+
+    
     <!--Select 2-->
     <link rel="stylesheet" type="text/css" href="{{ asset('plugins/select2/dist/css/select2.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{ asset('plugins/select2/dist/css/select2-bootstrap4.min.css')}}">
@@ -44,7 +49,7 @@
     <script>
         CKEDITOR.replace( 'article-ckeditor' );
     </script>
-
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.16/af-2.2.2/datatables.min.js"></script>
     <!--Assign User form script-->
     <script type="text/javascript">
       $(document).ready(function(){

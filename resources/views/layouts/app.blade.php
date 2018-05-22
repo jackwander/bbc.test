817395a461f5a60f1bb64fa3d98a9072a591701c
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-05-22 12:04:51 +0800

    Create JS for create form of userbranches.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,6 +43,146 @@
     <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
     <script>
         CKEDITOR.replace( 'article-ckeditor' );
-    </script>     
+    </script>
+
+    <!--Assign User form script-->
+    <script type="text/javascript">
+      $(document).ready(function(){
+        $('.select').select2();
+
+        $(document).on('change','#locations',function(){      
+          var loc_id = $(this).val();
+          var bank_id = $('#banks').val();
+
+          $('#branches').children().remove();
+          $('#branches').append($('<option>', {
+             text: 'Select Branch',
+             selected: true,
+             disabled: true
+          }));
+
+          $.ajax({
+            type:'GET',
+            url:'{!!URL::to('findBranches')!!}',
+            data:{
+              'loc_id':loc_id,
+              'bank_id':bank_id
+            },
+            success:function(data){
+              if(data.length>0) {
+                $('#branches').children().remove();
+                $('#branches').append($('<option>', {
+                   text: 'Select Branch',
+                   selected: true,
+                   disabled: true
+                }));            
+                
+                for(var i=0; i< data.length; i++) {
+                  $('#branches').append($('<option>', { 
+                      value: data[i].branch_id,
+                      text : data[i].branch_name
+                  }));            
+                }
+              }else {
+                $('#branches').children().remove();
+                $('#branches').append($('<option>', {
+                   text: 'No Branch Found',
+                   selected: true,
+                   disabled: true
+                }));
+              } 
+            },
+            error:function(){
+
+            }
+          });      
+        });
+
+        $(document).on('change','#banks',function(){      
+          var bank_id = $(this).val();
+          var loc_id = $('#locations').val();
+
+          $.ajax({
+            type:'GET',
+            url:'{!!URL::to('findBranches')!!}',
+            data:{
+              'loc_id':loc_id,
+              'bank_id':bank_id
+            },
+            success:function(data){
+              if(data.length>0) {
+                $('#branches').children().remove();
+                $('#users').children().remove();            
+                $('#branches').append($('<option>', {
+                   text: 'Select Branch',
+                   selected: true,
+                   disabled: true
+                }));            
+                
+                for(var i=0; i< data.length; i++) {
+                  $('#branches').append($('<option>', { 
+                      value: data[i].branch_id,
+                      text : data[i].branch_name
+                  }));            
+                }
+              }else {
+                $('#branches').children().remove();
+                $('#branches').append($('<option>', {
+                   text: 'No Branch Found',
+                   selected: true,
+                   disabled: true
+                }));
+              }          
+            },
+            error:function(){
+
+            }
+          });
+        });
+
+        $(document).on('change','#branches',function(){
+          var branch_id = $(this).val();
+          console.log(branch_id);          
+
+          $.ajax({
+            type:'GET',
+            url:'{!!URL::to('findUserBranches')!!}',
+            data:{
+              'branch_id':branch_id
+            },
+            success:function(data){
+              if(data.length>0) {
+                $('#users').children().remove();
+                $('#users').append($('<option>', {
+                   text: 'Select User',
+                   selected: true,
+                   disabled: true
+                }));            
+                
+                for(var i=0; i< data.length; i++) {
+                  var mname = data[i].mname;
+                  $('#users').append($('<option>', { 
+                      value: data[i].id,
+                      text : [data[i].fname+' '+mname.charAt(0)+'. '+data[i].lname]
+                  }));            
+                }
+              }else {
+                $('#users').children().remove();
+                $('#users').append($('<option>', {
+                   text: 'No Users Found',
+                   selected: true,
+                   disabled: true
+                }));
+              }          
+            },
+            error:function(){
+
+            }
+          });      
+        });
+
+      });
+    </script>
+    <!--Assign User form script-->             
 </body>
 </html>
